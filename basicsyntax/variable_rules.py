c642cc6ee6bb9bd39649206605b205e17ab7cc20
lens <lens.huygh@gmail.com>
2019-02-07 23:36:58 +0100

    added variable rules file

---
+++ basicsyntax/variable_rules.py
@@ -0,0 +1,26 @@
+import keyword
+
+print(keyword.kwlist)
+
+"""
+use a keyword as a var name doesn't work
+and = 10
+print(and)
+"""
+
+"""
+special characters in name
+variable_na$e = 10
+print(variable_na$e)
+"""
+print('-------')
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+print('-------')
+x, y, z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+print('-------')
