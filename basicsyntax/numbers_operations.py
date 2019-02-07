c23f1e760086923b6101a40a8c6a2e18a3856b14
lens <lens.huygh@gmail.com>
2019-02-07 23:50:39 +0100

    added numbers_operations.py

---
+++ basicsyntax/numbers_operations.py
@@ -0,0 +1,20 @@
+# one line comment in python
+
+# exponentiation
+exponents = 10 ** 3
+print(exponents)
+exp = 10 ** 20
+print(exp)
+
+
+"""
+this is a multiline / block comment in python
+"""
+
+# modulus
+remainder = 10 % 3
+print(remainder)
+result = 10 / 3
+print(result)
+another_remainder = 11 % 3
+print(another_remainder)
