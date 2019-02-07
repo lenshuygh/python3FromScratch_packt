e3e0f3ad30ef75b9321d3782e756cbbe4db364a6
lens <lens.huygh@gmail.com>
2019-02-07 23:44:35 +0100

    added numbers.py

---
+++ basicsyntax/numbers.py
@@ -0,0 +1,25 @@
+"""
+defining basic types
+"""
+
+int_num = 10
+
+float_number = 20.0
+
+print(int_num)
+print(float_number)
+
+a = 10
+b = 20
+print('adding')
+add = a + b
+print(add)
+print('subtracting')
+sub = a - b
+print(sub)
+print("multiplication")
+multiplication = a * b
+print(multiplication)
+print('division')
+divisi = b / a
+print(divisi)
