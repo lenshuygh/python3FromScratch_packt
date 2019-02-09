c443ac20045bc828a95a7f638b817a3f18898d3c
lens <lens.huygh@gmail.com>
2019-02-09 14:53:47 +0100

    added string formatting

---
+++ basicsyntax/string_formatting.py
@@ -0,0 +1,12 @@
+city = "NYC"
+event = "show"
+print("Welcome to " + city + " and enjoy the " + event)
+print()
+
+print("Welcome to %s and enjoy the %s" %(city,event))
+print("Welcome to %s" %(city))
+print("Welcome to %s" %city)
+print()
+
+num = 800
+print("The number is %f" % num)
