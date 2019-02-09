70e4e40554b952e13fa56466d642f93ab23c322c
lens <lens.huygh@gmail.com>
2019-02-09 23:51:33 +0100

    added methods_demo1

---
+++ methods/methods_demo1.py
@@ -0,0 +1,21 @@
+a = 3 + 2
+print(a)
+print('-*' * 50)
+
+
+def sum_nu():
+    print(2 + 3)
+
+
+sum_nu()
+
+print('-*' * 50)
+
+
+def sum_nums(n1, n2):
+    print(n1 + n2)
+
+
+sum_nums(10, 20)
+print('-*' * 50)
+
