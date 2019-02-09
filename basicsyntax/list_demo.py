96e30bb714baeff4639fc88840f721549a0e87b3
lens <lens.huygh@gmail.com>
2019-02-09 15:06:53 +0100

    added lists intro

---
+++ basicsyntax/list_demo.py
@@ -0,0 +1,23 @@
+cars = ['BMW',"Honda","VW",'Volvo']
+print(cars)
+print(cars[1])
+empty_list = []
+print(empty_list)
+print("*#"*20)
+print(cars[3])
+print(len(cars[3]))
+print(cars[3][1:-1])
+print("*#"*20)
+
+num_list = [1,2,3]
+sum_num = num_list[0] + num_list[1] + num_list[2]
+print(sum_num)
+print("*#"*20)
+
+
+cars[1] = "Kia"
+print(cars)
+print("*#"*20)
+
+
+
