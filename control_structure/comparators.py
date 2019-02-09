aa4d83fb944e1a4c140fc7a21f7950a5ecfd294e
lens <lens.huygh@gmail.com>
2019-02-09 19:44:24 +0100

    added comparators

---
+++ control_structure/comparators.py
@@ -0,0 +1,36 @@
+a = 100
+b = 50
+c = 100
+print(a == a)
+print(a == b)
+print(a == c)
+print("-"*50)
+
+print(a != a)
+print(a != b)
+print(a != c)
+print("-"*50)
+
+bool_one = 10 == 10
+print(bool_one)
+print("-"*50)
+
+bool_one = 10 == 11
+print(bool_one)
+not_equal = 10 != 11
+print(not_equal)
+less_than = 10 < 11
+print(less_than)
+greather_than = 10 > 9
+print(greather_than)
+lt_eq = 10 <= 10
+print(lt_eq)
+print("-"*50)
+
+lt_eq = 10 <= 11
+print(lt_eq)
+print("-"*50)
+
+lt_eq = 10 <= 9
+print(lt_eq)
+print("-"*50)
