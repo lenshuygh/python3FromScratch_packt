69d406fbe1bef54526505cf565cb84fdb1b143d8
lens <lens.huygh@gmail.com>
2019-02-09 21:49:58 +0100

    added conditional logic

---
+++ control_structure/conditional_logic.py
@@ -0,0 +1,32 @@
+if 100 == 10:
+    print('Hundred is  greater than 10!')
+print('This will print because not indented')
+
+print('-'*50)
+value = 'green'
+
+if value == 'green':
+    print("go")
+if value == 'red':
+    print('stop')
+print('this is not indented so it will always be printed')
+print('-'*50)
+
+
+value = 'green'
+if value == 'green':
+    print('Go')
+else:
+    print('NOT Go')
+
+print('-'*50)
+
+value = 'red'
+if value == 'green':
+    print('it\'s green')
+elif value == 'red':
+    print('it\'s red')
+else:
+    print("it's another color")
+print('-'*50)
+
