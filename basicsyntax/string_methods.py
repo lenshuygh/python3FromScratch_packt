49f0e568d2aae367cf0dd9ca436743740d7f62d0
lens <lens.huygh@gmail.com>
2019-02-09 14:15:29 +0100

    added strings and string methods1

---
+++ basicsyntax/string_methods.py
@@ -0,0 +1,27 @@
+# accessing first char in a string
+first = "nyc"[1]
+print(first)
+print()
+
+city = "sfo"
+print(city)
+f = city[0]
+print(f)
+print()
+
+t = "lens"
+l = len(t)
+print(l)
+print(len(t))
+print(len("thorreke"))
+print()
+
+s = "this is a Mixed Case"
+print(s)
+print(s.lower())
+print(s.upper())
+print(len(s))
+print(s + str(2))
+print()
+
+print("hello " + " " + " world !!!")
---
+++ basicsyntax/strings.py
@@ -0,0 +1,25 @@
+a = "This is a simple string"
+b = 'Using single quotes'
+print(a)
+print(b)
+print()
+
+c = "need to use 'single quotes' in a string"
+print(c)
+d = 'need to use "double quotes" in a string'
+print(d)
+print()
+
+print("use \\ as an escape sign")
+c = 'need to use \'single quotes\' in a string'
+print(c)
+d = "need to use \"double quotes\" in a string"
+print(d)
+print()
+
+print('use \\ to split a line')
+c = "this\
+ is a \
+string"
+print(c)
+
