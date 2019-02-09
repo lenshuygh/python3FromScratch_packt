44363af1e368eec7d3cf54a3df6624e91ee929b7
lens <lens.huygh@gmail.com>
2019-02-09 22:39:22 +0100

    added while_demo

---
+++ control_structure/while_demo.py
@@ -0,0 +1,16 @@
+x = 0
+
+while x < 10:
+    print("the value of x is %d." %x)
+    x = x + 1
+print('--'*50)
+
+l = []
+num = 0
+while num < 10:
+    l.append(num)
+    num += 1
+print(l)
+print('--'*50)
+
+print('--'*50)
