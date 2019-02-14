b98ec7e20cf8439549cd69ecba44bbeaacc638ca
Lens Huygh <lens.huygh@gmail.com>
2019-02-14 14:49:46 +0100

    added class_demo_1 intro to classes - instances

---
+++ classes_and_objects/class_demo_1.py
@@ -0,0 +1,14 @@
+"""
+OOP
+"""
+s = "This is an object"
+print(type(s))
+print(s.upper())
+print(s.lower())
+print("*"*50)
+print(type(s))
+print(type(10))
+print(type([1,2]))
+print(type((1,2)))
+print(type({"1":1}))
+
